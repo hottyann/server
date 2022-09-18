2eba2ea224afd210c91a348e478460b91a06ed88
hottyann <61652927+hottyann@users.noreply.github.com>
2022-09-18 17:55:06 +0900

    make it is

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
-import { Client, Intents } from 'discord.js'
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const Discord = require('discord.js')
+const client = new Discord.Client({
+  intents: ['GUILDS', 'GUILD_MESSAGES']
+})
 
-import axios from 'axios'
-const baseUrl = process.env.LAMBDA_INVOKE_PATH
-
-client.once("ready", async () => {
-  console.log("Ready!");
+client.on('ready', client => {
+  console.log(`Logged in as ${client.user.tag}!`)
 })
 
 client.on('messageCreate', message => {
-  // 起動
-  if (message.content === '!mcstart') {
-    axios.get(`${baseUrl}ec2-up`)
-      .then(res => {
-        message.reply(`マイクラサーバを起動しました。IPは${res.data}です。`)
-      })
-      .catch(err => {
-        message.reply('既に起動しています。')
-      })
+  if (message.content === 'ping') {
+    message.reply('Pong!')
+      .catch(console.error)
   }
-
-  // 停止
-if (message.content === '!mcstop') {
-  axios.get(`${baseUrl}ec2-down`)
-    .then(res => {
-      message.reply(`マイクラサーバを停止しました。`)
-  })
-  .catch(err => {
-    message.reply('既に停止しています。')
-  })
-}
-
 })
 
-client.login(process.env.DISCORD_TOKEN)
+client.login()
+  .catch(console.error)
